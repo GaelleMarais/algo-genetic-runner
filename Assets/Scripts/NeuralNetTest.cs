e3ef39699b78d82592446737de3ec27a3a03537e
antoinechedin <antoine.chedin@gmail.com>
2019-11-21 13:46:08 +0100

    add neural network class

---
+++ Assets/Scripts/NeuralNetTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNetTest : MonoBehaviour
+{
+    public NeuralNetwork network;
+
+    private void Start()
+    {
+        network = new NeuralNetwork(1, 1, 1);
+        network.weightList[0][0,0] = 0.5f;
+        network.weightList[0][1,0] = 0.2f;
+        network.weightList[1][0,0] = -1f;
+        network.weightList[1][1,0] = 0f;
+
+        float[] inputs = new float[1];
+        inputs[0] = 1f;
+
+        network.Compute(inputs);
+    }
+
+}
---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNetwork
+{
+    public List<float[,]> weightList;
+
+    public int numInput;
+    public int numHidden;
+    public int numOutput;
+
+    public NeuralNetwork(int numInput, int numHidden, int numOutput)
+    {
+        this.numInput = numInput;
+        this.numHidden = numHidden;
+        this.numOutput = numOutput;
+
+        weightList = new List<float[,]>();
+        weightList.Add(new float[numInput + 1, numHidden]);
+        weightList.Add(new float[numHidden + 1, numOutput]);
+    }
+
+    public float[] Compute(float[] inputs)
+    {
+        float[] hidden = new float[numHidden];
+        // Mat mult
+        for (int i = 0; i < numHidden; i++)
+        {
+            for (int j = 0; j < numInput; j++)
+            {
+                hidden[i] += inputs[j] * weightList[0][j, i];
+            }
+            hidden[i] += weightList[0][numInput, i];
+        }
+        // Sigmoid 
+        hidden = Sigmoid(hidden);
+
+        float[] output = new float[numOutput];
+        // Mat mult
+        for (int i = 0; i < numOutput; i++)
+        {
+            for (int j = 0; j < numHidden; j++)
+            {
+                output[i] += hidden[j] * weightList[1][j, i];
+            }
+            output[i] += weightList[1][numHidden, i];
+        }
+        // Sigmoid 
+        output = Sigmoid(output);
+
+        return output;
+    }
+
+    private static float[] Sigmoid(float[] values)
+    {
+        float[] res = new float[values.Length];
+        for (int i = 0; i < res.Length; i++)
+        {
+            res[i] = 1.0f / (1.0f + Mathf.Exp(values[i]));
+        }
+        return res;
+    }
+}
