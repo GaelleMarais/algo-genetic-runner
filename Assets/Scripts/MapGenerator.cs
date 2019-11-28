1663953d05bcfb77e100d178b483f5103e3ea9eb
GaelleMarais <marais.gaelle@gmail.com>
2019-11-28 12:36:37 +0000

    Add function to clear the map

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,51 +5,27 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
 
-    public int numPiece;
-    public int obstacleFrequence;
-
-    public GameObject piece000;
-    public GameObject piece001;
-    public GameObject piece010;
-    public GameObject piece011;
-    public GameObject piece100;
-    public GameObject piece101;
-    public GameObject piece110;
+    public int size;
+    public GameObject[] pieces;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < numPiece; i++){
-            int rand = Random.Range(0, obstacleFrequence + 6);
-            switch (rand){
-                case 0: 
-                    Instantiate(piece001, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                case 1:
-                    Instantiate(piece010, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(piece011, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(piece100, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                case 4:
-                    Instantiate(piece101, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                case 5:
-                    Instantiate(piece110, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-                default:
-                    Instantiate(piece000, new Vector3(-i * 4, 0, 0), Quaternion.identity);
-                    break;
-            }
+        Create();
+    }
+
+    void Clear(){
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Create(){
+        for(int i = 0; i < size; i++){
+            int rand = Random.Range(0, pieces.Length - 1);
+
+            var newPiece = Instantiate(pieces[rand], new Vector3(-i * 4, 0, 0), Quaternion.identity);
+            newPiece.transform.parent = gameObject.transform;                   
+        }
     }
 }
